883a469e526d63c54b52660d237d2b0d2fddd689
daHil <ssocmain@gmail.com>
2024-03-07 17:55:37 +0500

    added implementation for IToHttpContentParser interface

---
+++ Core/HttpLogic/HttpRequests/Parsers/ContentTypeParser.cs
@@ -0,0 +1,22 @@
+﻿using Core.HttpLogic.HttpRequests.Parsers.ContentTypeParsers;
+
+namespace Core.HttpLogic.HttpRequests.Parsers
+{
+    internal class ContentTypeParser : IToHttpContentParser<ContentType>
+    {
+        private readonly IEnumerable<IContentTypeParser> parsers;
+
+        public ContentTypeParser(IEnumerable<IContentTypeParser> parsers)
+        {
+            this.parsers = parsers;
+        }
+
+        public HttpContent Parse(object body, ContentType contentType)
+        {
+            var parser = parsers.First(parser => parser.SupportedContentType == contentType)
+                ?? throw new ArgumentOutOfRangeException(nameof(contentType), contentType, null);
+
+            return parser.Parse(body);
+        }
+    }
+}
