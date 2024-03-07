8c55800588e508d4e5aa3e9fdd805698281a5d4b
daHil <ssocmain@gmail.com>
2024-03-07 20:26:52 +0500

    added http policy interface

---
+++ Core/HttpLogic/Polly/IHttpPolicy.cs
@@ -0,0 +1,13 @@
+﻿using Polly;
+
+namespace Core.HttpLogic.Polly
+{
+    internal interface IHttpPolicy
+    {
+        IAsyncPolicy GetRetryPolicy(
+            int retryCount = 3,
+            int retryIntervalInSeconds = 5);
+
+        IAsyncPolicy GetTimeoutPolicy(TimeSpan timeout);
+    }
+}
