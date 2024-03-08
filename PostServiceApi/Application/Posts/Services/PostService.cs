f7b753c53d8a693b327b84f4ecd455e21ff461cd
daHil <ssocmain@gmail.com>
2024-03-09 02:26:57 +0500

    get methods now return view models with user info in them

--- PostServiceApi/Application/Posts/Services/PostService.cs
+++ PostServiceApi/Application/Posts/Services/PostService.cs
@@ -1,4 +1,7 @@
-﻿using Application.Posts.Mappers;
+﻿using Application.Posts.Mappers.InputModelMapper;
+using Application.Posts.Mappers.ViewModelMapper;
+using Application.Posts.Models;
+using Domain.Clients.PostUsersInfo;
 using Domain.Posts;
 using Domain.Posts.Exceptions;
 
@@ -7,17 +10,25 @@ namespace Application.Posts.Services
     public sealed class PostService : IPostService
     {
         private readonly IPostRepository postRepository;
-        private readonly IPostViewModelMapper postMapper;
+        private readonly IPostInputModelMapper postInputModelMapper;
+        private readonly IPostViewModelMapper postViewModelMapper;
+        private readonly IPostUserInfoServiceClient postUserInfoServiceClient;
 
-        public PostService(IPostRepository postRepository, IPostViewModelMapper postMapper)
+        public PostService(
+            IPostRepository postRepository,
+            IPostInputModelMapper postMapper, 
+            IPostUserInfoServiceClient postUserInfoServiceClient,
+            IPostViewModelMapper postViewModelMapper)
         {
             this.postRepository = postRepository;
-            this.postMapper = postMapper;
+            this.postInputModelMapper = postMapper;
+            this.postUserInfoServiceClient = postUserInfoServiceClient;
+            this.postViewModelMapper = postViewModelMapper;
         }
 
-        public async Task<Guid> CreateAsync(PostViewModel viewModel)
+        public async Task<Guid> CreateAsync(PostInputModel inputModel)
         {
-            var entity = postMapper.Map(viewModel with { Id = Guid.Empty });
+            var entity = postInputModelMapper.Map(inputModel with { Id = Guid.Empty });
 
             return await postRepository.CreateAsync(entity);
         }
@@ -30,24 +41,27 @@ namespace Application.Posts.Services
         public async Task<PostViewModel> GetAsync(Guid id)
         {
             var entity = await postRepository.GetAsync(id) ?? throw new PostNotFoundException(id);
-            var viewModel = postMapper.Map(entity);
+            var userInfo = await postUserInfoServiceClient.GetPostUsersInfoAsync([id]);
+            var viewModel = postViewModelMapper.Map(entity);
 
-            return viewModel;
+            return viewModel with { PostUserInfo = userInfo.First()};
         }
 
         public async Task<IEnumerable<PostViewModel>> GetPageAsync(int pageNumber, int pageSize)
         {
             var page = await postRepository.GetPageAsync(pageNumber, pageSize);
+            var usersId = page.Select(post => post.Id).ToArray();
+            var usersInfo = await postUserInfoServiceClient.GetPostUsersInfoAsync(usersId);
 
-            return page.Select(postMapper.Map);
+            return page.Zip(usersInfo, (post, userInfo) => postViewModelMapper.Map(post) with { PostUserInfo = userInfo});
         }
 
-        public async Task UpdateAsync(PostViewModel viewModel)
+        public async Task UpdateAsync(PostInputModel inputModel)
         {
-            if(await postRepository.GetAsync(viewModel.Id) is null)
-                throw new PostNotFoundException(viewModel.Id);
+            if(await postRepository.GetAsync(inputModel.Id) is null)
+                throw new PostNotFoundException(inputModel.Id);
 
-            var entity = postMapper.Map(viewModel);
+            var entity = postInputModelMapper.Map(inputModel);
             await postRepository.UpdateAsync(entity);
         }
     }
