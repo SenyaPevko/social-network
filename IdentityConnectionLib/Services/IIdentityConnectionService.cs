aa40a954ba441928865563956b8f2e5db16e77d9
daHil <ssocmain@gmail.com>
2024-03-08 21:32:05 +0500

    added interface for identity api connection service

---
+++ IdentityConnectionLib/Services/IIdentityConnectionService.cs
@@ -0,0 +1,11 @@
+﻿using IdentityConnectionLib.DtoModels.ProfileInfo;
+using IdentityConnectionLib.DtoModels.UserInfoLists;
+
+namespace IdentityConnectionLib.Services
+{
+    internal interface IIdentityConnectionService
+    {
+        Task<UserInfoListIdentityServiceApiResponse> GetUserInfoListAsync(UserInfoListIdentityServiceApiRequest request);
+        Task<ProfileInfoListIdentityServiceApiResponse> GetProfileInfoListAsync(ProfileInfoListIdentityServiceApiRequest request);
+    }
+}
