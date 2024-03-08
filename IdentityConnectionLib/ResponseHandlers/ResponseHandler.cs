e76cb04a37b507753b71ae12347e0868a62060c7
daHil <ssocmain@gmail.com>
2024-03-08 21:34:17 +0500

    added implementation for response handler interface

---
+++ IdentityConnectionLib/ResponseHandlers/ResponseHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace IdentityConnectionLib.ResponseHandlers
+{
+    public class ResponseHandler : IResponseHandler
+    {
+        public void HandleErrorResponse(HttpStatusCode statusCode)
+        {
+            switch (statusCode)
+            {
+                default:
+                    throw new HttpRequestException($"Request failed with status code: {statusCode}");
+            }
+        }
+    }
+}
