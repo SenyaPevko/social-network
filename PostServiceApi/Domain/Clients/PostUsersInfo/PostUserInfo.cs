91cc2c12175dc34df79b216bfa814d64d55b5426
daHil <ssocmain@gmail.com>
2024-03-09 02:24:59 +0500

    added client interface to fetch user info data

---
+++ PostServiceApi/Domain/Clients/PostUsersInfo/PostUserInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Clients.PostUsersInfo
+{
+    public record PostUserInfo
+    {
+        /// <summary>
+        /// Usser's first name
+        /// </summary>
+        public required string FirstName { get; init; }
+
+        /// <summary>
+        /// Usser's second name
+        /// </summary>
+        public required string SecondName { get; init; }
+
+        /// <summary>
+        /// User's status
+        /// </summary>
+        public required string Status { get; init; }
+
+        /// <summary>
+        /// User's avatar
+        /// </summary>
+        public required string Avatar { get; init; }
+    }
+}
