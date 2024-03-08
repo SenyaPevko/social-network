59d0476d7d45152aee0958ab41652d9e39d67bfa
daHil <ssocmain@gmail.com>
2024-03-08 21:31:19 +0500

    added base request model

---
+++ IdentityConnectionLib/DtoModels/BaseRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace IdentityConnectionLib.DtoModels
+{
+    public record BaseRequest
+    {
+        /// <summary>
+        /// Amount of retries that will be made in case if request won't be succesfull
+        /// </summary>
+        public int RetryCount { get; set; }
+
+        /// <summary>
+        /// Interval in seconds that will be awaited before the next request retry call
+        /// </summary>
+        public TimeSpan RetryInterval { get; set; }
+
+        /// <summary>
+        /// Interval in seconds that will be awaited before the next request retry call
+        /// </summary>
+        public TimeSpan ResponseAwaitTime { get; set; }
+    }
+}
