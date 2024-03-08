891ef630cf3b6506a1a8e22fcfa6affa34c7b395
daHil <ssocmain@gmail.com>
2024-03-09 02:22:15 +0500

    added view model mapper

---
+++ PostServiceApi/Application/Posts/Mappers/ViewModelMapper/IPostViewModelMapper.cs
@@ -0,0 +1,10 @@
+﻿using Application.Interfaces;
+using Application.Posts.Models;
+using Domain.Posts;
+
+namespace Application.Posts.Mappers.ViewModelMapper
+{
+    public interface IPostViewModelMapper : IMapper<PostViewModel, Post>
+    {
+    }
+}
