9e13f64e20f782f7d2a14790ddaffb2b22b50829
daHil <ssocmain@gmail.com>
2024-03-10 18:33:07 +0500

    added di for http services

---
+++ Core/HttpLogic/HttpServiceStartup.cs
@@ -0,0 +1,28 @@
+﻿using Core.HttpLogic.HttpConnections.Services;
+using Core.HttpLogic.HttpRequests.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Core.HttpLogic
+{
+    /// <summary>
+    /// Registrating http services into di container
+    /// </summary>
+    public static class HttpServiceStartup
+    {
+        /// <summary>
+        /// Adding services to send requests by http protocol
+        /// </summary>
+        public static IServiceCollection AddHttpServices(this IServiceCollection services)
+        {
+            services
+                .AddHttpContextAccessor()
+                .AddHttpClient()
+                .AddTransient<IHttpConnectionService, HttpConnectionService>();
+            
+            services.TryAddTransient<IHttpRequestService, HttpRequestService>();
+
+            return services;
+        }
+    }
+}
