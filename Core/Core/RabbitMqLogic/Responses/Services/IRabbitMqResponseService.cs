a9f0cc1bd5f6e7f7b4c078fcd40f6db1d0903464
daHil <ssocmain@gmail.com>
2024-03-14 02:05:24 +0500

    added interface for RabbiMq server side functionality

---
+++ Core/Core/RabbitMqLogic/Responses/Services/IRabbitMqResponseService.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.RabbitMqLogic.Responses.Services
+{
+    /// <summary>
+    /// Service for servers to read message from their request queues and 
+    /// publish messages to their queue using RabbitMQ based on the RPC pattern
+    /// </summary>
+    public interface IRabbitMqResponseService : IDisposable
+    {
+        /// <summary>
+        /// Start listening to the selected queue and send responses
+        /// </summary>
+        /// <param name="handleRequest"></param>
+        void StartListeningForRequests(Func<string, string> handleRequest);
+    }
+}
