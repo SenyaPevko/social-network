b7a820596a71dd52be6fa21324c05023e96957d0
daHil <ssocmain@gmail.com>
2024-03-14 01:54:11 +0500

    added implementation for id generator interface

---
+++ Core/Core/RabbitMqLogic/Geneartors/Ids/IdGenerator.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.RabbitMqLogic.Geneartors.Ids
+{
+    /// <inheritdoc />
+    public class IdGenerator : IIdGenerator
+    {
+        /// <inheritdoc />
+        public string GenerateCorrelationId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ Core/Core/RabbitMqLogic/Geneartors/QueueNames/QueueNameGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.RabbitMqLogic.Geneartors.QueueNames
+{
+    /// <inheritdoc />
+    public class QueueNameGenerator : IQueueNameGenerator
+    {
+        /// <inheritdoc />
+        public string GenerateRequestQueueName<TModel>()
+        {
+            return $"Request{nameof(TModel)}";
+        }
+
+        /// <inheritdoc />
+        public string GenerateResponseQueueName()
+        {
+            return $"Response{Guid.NewGuid()}";
+        }
+    }
+}
