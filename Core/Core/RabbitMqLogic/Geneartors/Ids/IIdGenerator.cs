aea4d9071f652395c513b92a36cc1c631f8a53f6
daHil <ssocmain@gmail.com>
2024-03-14 01:53:37 +0500

    added interface for generating id functionality

---
+++ Core/Core/RabbitMqLogic/Geneartors/Ids/IIdGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.RabbitMqLogic.Geneartors.Ids
+{
+    /// <summary>
+    /// Id generator
+    /// </summary>
+    public interface IIdGenerator
+    {
+        /// <summary>
+        /// Generates correlation id for RabbitMq requests
+        /// </summary>
+        /// <returns></returns>
+        string GenerateCorrelationId();
+    }
+}
