2bcaf6ad7f087eab99f7c432bcc5a778ca4bc2a3
daHil <ssocmain@gmail.com>
2024-03-15 18:07:27 +0500

    added types of enum connection

---
+++ Core/Core/Logic/Connections/RabbitMqLogic/Connections/Models/RabbitMqConnectionType.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Logic.Connections.RabbitMqLogic.Connections.Models
+{
+    /// <summary>
+    /// RabbitMq connection type
+    /// </summary>
+    public enum RabbitMqConnectionType
+    {
+        /// <summary>
+        /// Connection to RabbitMq by uri
+        /// </summary>
+        Uri = 0,
+
+        /// <summary>
+        /// Connection to RabbitMq by uri host name
+        /// </summary>
+        Host = 1,
+    }
+}
