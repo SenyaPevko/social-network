19b20c439812411af8042136cdef93510182267f
daHil <ssocmain@gmail.com>
2024-03-15 18:11:24 +0500

    added interface for recieving requests from client and sending responses back

---
+++ Core/Core/Logic/Connections/RabbitMqLogic/Responses/Services/IRabbitMqResponseService.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Logic.Connections.RabbitMqLogic.Responses.Services
+{
+    /// <summary>
+    /// Service for servers to read message from their request queues and 
+    /// publish messages to their queue using RabbitMQ based on the RPC pattern
+    /// </summary>
+    public interface IRabbitMqResponseService : IDisposable
+    {
+        /// <summary>
+        /// Start listening to the selected queue and send responses
+        /// </summary>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <typeparam name="TResponse"></typeparam>
+        /// <param name="handleRequest"></param>
+        public void StartListeningForRequests<TRequest, TResponse>(Func<TRequest, Task<TResponse>> handleRequest);
+    }
+}
