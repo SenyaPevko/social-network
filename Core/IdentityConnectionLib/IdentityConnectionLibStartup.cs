545e309977c58970dd8b03730ba7df9372504b27
daHil <ssocmain@gmail.com>
2024-03-15 18:20:50 +0500

    added RabbitMq services to di container extension

--- Core/IdentityConnectionLib/IdentityConnectionLibStartup.cs
+++ Core/IdentityConnectionLib/IdentityConnectionLibStartup.cs
@@ -1,25 +1,45 @@
-﻿using IdentityConnectionLib.Config.Models;
+﻿using Core.Logic.Connections.Base;
+using IdentityConnectionLib.Config.Models;
 using IdentityConnectionLib.ResponseHandlers;
-using IdentityConnectionLib.Services;
+using IdentityConnectionLib.Services.Http;
+using IdentityConnectionLib.Services.Identity;
+using IdentityConnectionLib.Services.RabbitMq;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace IdentityConnectionLib;
-
-public static class IdentityConnectionLibStartup
+namespace IdentityConnectionLib
 {
-    public static IServiceCollection AddIdentityConnectionLibServices(this IServiceCollection services)
+    /// <summary>
+    /// Registrating Identity api connection services into di container
+    /// </summary>
+    public static class IdentityConnectionLibStartup
     {
-        services.ConfigureConnections();
-        services.AddScoped<IIdentityConnectionService, IdentityConnectionService>();
-        services.AddScoped<IResponseHandler, ResponseHandler>();
+        /// <summary>
+        /// Add services to connect to the identity api
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddIdentityConnectionLibServices(this IServiceCollection services)
+        {
+            services.ConfigureConnections();
+            services.AddScoped<IIdentityConnectionService, IdentityConnectionService>();
+            services.AddScoped<IRabbitMqConnectionService, RabbitMqConnectionService>();
+            services.AddScoped<IHttpConnectionService, HttpConnectionService>();
+            services.AddScoped<IResponseHandler, ResponseHandler>();
 
-        return services;
-    }
+            return services;
+        }
 
-    public static IServiceCollection ConfigureConnections(this IServiceCollection services)
-    {
-        services.AddSingleton<IIdentityApiConnectionConfig, IdentityApiConnectionConfig>();
+        /// <summary>
+        /// Add connections configuration  
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection ConfigureConnections(this IServiceCollection services)
+        {
+            services.AddSingleton<IIdentityApiConnectionConfig, IdentityApiConnectionConfig>();
+            services.AddSingleton<IConnectionConfiguration, IdentityApiConnectionConfig>();
 
-        return services;
+            return services;
+        }
     }
 }
