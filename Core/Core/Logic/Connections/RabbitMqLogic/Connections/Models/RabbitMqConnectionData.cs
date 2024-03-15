86e918ad85e14a89dcbf95d0fbd5cdb18adafaf5
daHil <ssocmain@gmail.com>
2024-03-15 18:07:05 +0500

    added data needed to connect to rabbit mq server

---
+++ Core/Core/Logic/Connections/RabbitMqLogic/Connections/Models/RabbitMqConnectionData.cs
@@ -0,0 +1,32 @@
+﻿using Core.Logic.Connections.Base;
+
+namespace Core.Logic.Connections.RabbitMqLogic.Connections.Models
+{
+    /// <summary>
+    /// Data that is needed to connect to RabbitMq
+    /// </summary>
+    public class RabbitMqConnectionData
+    {
+        /// <summary>
+        /// RabbitMq connection type
+        /// </summary>
+        public RabbitMqConnectionType ConnectionType { get; set; }
+
+        /// <summary>
+        /// AmqpUrl to connect to RabbitMq server
+        /// </summary>
+        public string AmqpUrl { get; }
+
+        /// <summary>
+        /// Host name to connect to RabbitMq
+        /// </summary>
+        public string HostName { get; }
+
+        public RabbitMqConnectionData(IConnectionConfiguration connectionConfiguration)
+        {
+            ConnectionType = connectionConfiguration.RabbitMqConnectionType;
+            AmqpUrl = connectionConfiguration.RabbitMqUrl;
+            HostName = connectionConfiguration.RabbitMqHostName;
+        }
+    }
+}
