88a21e909719e7da1be81d4cce98ec8ff065d3d3
daHil <ssocmain@gmail.com>
2024-03-15 18:04:10 +0500

    added implementation of interface for generating id

---
+++ Core/Core/Logic/Connections/RabbitMqLogic/Generators/Ids/IdGenerator.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Logic.Connections.RabbitMqLogic.Geneartors.Ids
+{
+    /// <inheritdoc />
+    public class IdGenerator : IIdGenerator
+    {
+        /// <inheritdoc />
+        public string GenerateCorrelationId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
