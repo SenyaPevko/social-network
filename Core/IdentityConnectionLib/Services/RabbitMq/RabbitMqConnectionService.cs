fef5e8f1e9e7f57939459dc2fb1804bab07c6480
daHil <ssocmain@gmail.com>
2024-03-15 18:17:45 +0500

    added implementation of interface for connecting by RabbitMq to identity api

---
+++ Core/IdentityConnectionLib/Services/RabbitMq/RabbitMqConnectionService.cs
@@ -0,0 +1,37 @@
+﻿using Core.Logic.Connections.RabbitMqLogic.Requests.Services;
+using IdentityConnectionLib.DtoModels.ProfileInfo;
+using IdentityConnectionLib.DtoModels.UserInfoLists;
+
+namespace IdentityConnectionLib.Services.RabbitMq
+{
+    /// <inheritdoc />
+    internal class RabbitMqConnectionService : IRabbitMqConnectionService
+    {
+        private readonly IRabbitMqRequestService rabbitMqRequestService;
+
+        public RabbitMqConnectionService(IRabbitMqRequestService rabbitMqRequestService)
+        {
+            this.rabbitMqRequestService = rabbitMqRequestService;
+        }
+
+        /// <inheritdoc />
+        public async Task<ProfileInfoListIdentityServiceApiResponse> GetProfileInfoListAsync(
+            ProfileInfoListIdentityServiceApiRequest request)
+        {
+            var response = await rabbitMqRequestService.SendRequestAsync<ProfileInfoListIdentityServiceApiRequest,
+                ProfileInfoListIdentityServiceApiResponse>(request);
+
+            return response;
+        }
+
+        /// <inheritdoc />
+        public async Task<UserInfoListIdentityServiceApiResponse> GetUserInfoListAsync(
+            UserInfoListIdentityServiceApiRequest request)
+        {
+            var response = await rabbitMqRequestService.SendRequestAsync<UserInfoListIdentityServiceApiRequest,
+                UserInfoListIdentityServiceApiResponse>(request);
+
+            return response;
+        }
+    }
+}
