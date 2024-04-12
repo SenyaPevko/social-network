a798fe1023a4ae6e927322f8e96c31665aeda1b7
daHil <ssocmain@gmail.com>
2024-04-13 02:18:39 +0500

    added PostConnectionLib with dto models

---
+++ Core/PostConnectionLib/DtoModels/PostInfo/PostInfo.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Tags;
+
+namespace PostConnectionLib.DtoModels.PostInfo;
+
+/// <summary>
+/// Represents info about the post
+/// </summary>
+public record PostInfo
+{
+    /// <summary>
+    /// Id of this post
+    /// </summary>
+    public Guid Id { get; init; }
+    
+    /// <summary>
+    /// Id of the user that created this post
+    /// </summary>
+    public Guid UserId { get; init; }
+
+    /// <summary>
+    /// Post content: text/images/videos etc
+    /// </summary>
+    public string Content { get; init; } = null!;
+
+    /// <summary>
+    /// Amount of likes on the post
+    /// </summary>
+    public int LikesCount {  get; init; }
+
+    /// <summary>
+    /// Amount of comments on the post
+    /// </summary>
+    public int CommentsCount { get; init; }
+
+    /// <summary>
+    /// The date and time when the post was created
+    /// </summary>
+    public DateTime CreatedAt { get; init; }
+
+    /// <summary>
+    /// The date and time when the post was last updated
+    /// </summary>
+    public DateTime UpdatedAt { get; init; }
+
+    /// <summary>
+    /// The tags that were asigned to the post
+    /// </summary>
+    public ICollection<Tag> Tags { get; init; } = null!;
+}
---
+++ Core/PostConnectionLib/DtoModels/PostInfo/PostInfoListPostServiceApiRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace PostConnectionLib.DtoModels.PostInfo;
+
+/// <summary>
+/// Represents a request model for obtaining information about a list of posts.
+/// </summary>
+public record PostInfoListPostServiceApiRequest
+{
+    /// <summary>
+    /// The array of post IDs for which information is requested.
+    /// </summary>
+    public Guid[] PostsId { get; set; }
+}
