c93d246068c47aafc51780ff4e621ec322280655
daHil <ssocmain@gmail.com>
2024-04-13 02:13:35 +0500

    added request of GetPostListSaga

---
+++ Core/Sagas/Posts/GetPost/Requests/GetPostListRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Sagas.Posts.GetPost.Requests;
+
+/// <summary>
+///     Represents a request model for obtaining information about a list of posts.
+/// </summary>
+public class GetPostListRequest
+{
+    /// <summary>
+    ///     The array of post IDs for which information is requested.
+    /// </summary>
+    public Guid[] PostsId { get; set; }
+
+    /// <summary>
+    ///     The correlation ID for the saga instance.
+    /// </summary>
+    public Guid CorrelationId { get; set; }
+}
