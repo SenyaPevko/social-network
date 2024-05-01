5860afcf79be6fc75bc3d069f0fe115fdeaa93f5
daHil <ssocmain@gmail.com>
2024-05-01 16:02:33 +0500

    added interface for a distributed lock

---
+++ Core/LockerLib/Locks/DistributedLocks/IDistributedLock.cs
@@ -0,0 +1,21 @@
+﻿namespace LockerLib.Locks.DistributedLocks;
+
+/// <summary>
+/// Interface for a distributed lock.
+/// </summary>
+public interface IDistributedLock : IDisposable
+{
+    /// <summary>
+    /// Tries to acquire the lock asynchronously.
+    /// </summary>
+    /// <param name="timeout">The timeout for acquiring the lock.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>True if the lock was successfully acquired; otherwise, false.</returns>
+    Task<bool> AcquireAsync(TimeSpan timeout = default, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Releases the lock asynchronously.
+    /// </summary>
+    /// <returns>True if the lock was successfully released; otherwise, false.</returns>
+    Task<bool> ReleaseAsync();
+}
