a16943f1854b646b9bdc7902c5dcf25bacec1b06
daHil <ssocmain@gmail.com>
2024-05-01 16:00:58 +0500

    added lock manager configuration

---
+++ Core/LockerLib/Locks/LockManagers/LockManagerConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace LockerLib.Locks.LockManagers;
+
+/// <summary>
+/// Configuration for the lock manager.
+/// </summary>
+public class LockManagerConfiguration
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LockManagerConfiguration"/> class with the specified maximum count.
+    /// </summary>
+    /// <param name="maxCount">The maximum count of locks.</param>
+    public LockManagerConfiguration(int maxCount)
+    {
+        MaxCount = maxCount;
+    }
+    
+    /// <summary>
+    /// Gets the maximum count of locks.
+    /// </summary>
+    public int MaxCount { get; }
+}
