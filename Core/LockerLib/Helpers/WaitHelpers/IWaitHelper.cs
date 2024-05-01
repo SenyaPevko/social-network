d59950fe8f4766c07f152142f805ec45691ed982
daHil <ssocmain@gmail.com>
2024-05-01 15:58:09 +0500

    added interface for waiting cancellation methods provider

---
+++ Core/LockerLib/Helpers/WaitHelpers/IWaitHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace LockerLib.Helpers.WaitHelpers;
+
+/// <summary>
+/// Provides methods for waiting cancellation.
+/// </summary>
+public interface IWaitHelper
+{
+    /// <summary>
+    /// Asynchronously waits for cancellation.
+    /// </summary>
+    /// <param name="combinedToken">The combined cancellation token.</param>
+    /// <param name="timeoutToken">The timeout cancellation token.</param>
+    /// <returns>A task representing the asynchronous operation, with a result indicating whether the cancellation was successful.</returns>
+
+    Task<bool> WaitCancellationAsync(CancellationToken combinedToken, CancellationToken timeoutToken);
+}
