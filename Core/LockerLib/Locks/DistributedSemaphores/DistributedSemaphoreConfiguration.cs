a43eba0e08988bfc0f11fc8d61ef7879ca0900bc
daHil <ssocmain@gmail.com>
2024-05-01 16:04:21 +0500

    added configuration settings for a distributed semaphore

---
+++ Core/LockerLib/Locks/DistributedSemaphores/DistributedSemaphoreConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace LockerLib.Locks.DistributedSemaphores;
+
+/// <summary>
+/// Configuration settings for a distributed semaphore.
+/// </summary>
+public class DistributedSemaphoreConfiguration
+{
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DistributedSemaphoreConfiguration"/> class.
+    /// </summary>
+    /// <param name="maxCount">The maximum count of the semaphore.</param>
+    /// <param name="name">The optional name of the semaphore.</param>
+    public DistributedSemaphoreConfiguration(int maxCount, string? name = null)
+    {
+        MaxCount = maxCount;
+        Name = name;
+    }
+
+    /// <summary>
+    /// Gets the maximum count of the semaphore.
+    /// </summary>
+    public int MaxCount { get; }
+    
+    /// <summary>
+    /// Gets the optional name of the semaphore.
+    /// </summary>
+    public string? Name { get; }
+}
